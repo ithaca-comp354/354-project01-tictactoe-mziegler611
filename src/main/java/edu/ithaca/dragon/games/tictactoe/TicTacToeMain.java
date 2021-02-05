b78118c61b8a96ad42359bc49eb93520526bcb2d
Morgan <morgan.e.ziegler@gmail.com>
2021-02-05 09:48:42 -0500

    Smarter Player Completed

--- src/main/java/edu/ithaca/dragon/games/tictactoe/TicTacToeMain.java
+++ src/main/java/edu/ithaca/dragon/games/tictactoe/TicTacToeMain.java
@@ -4,11 +4,12 @@ import edu.ithaca.dragon.games.tictactoe.player.BrainDeadPlayer;
 import edu.ithaca.dragon.games.tictactoe.player.HumanPlayer;
 import edu.ithaca.dragon.games.tictactoe.player.RandomMovePlayer;
 import edu.ithaca.dragon.games.tictactoe.player.RandomSmarterPlayer;
+import edu.ithaca.dragon.games.tictactoe.player.SmartPlayer;
 
 public class TicTacToeMain {
     public static void main (String[] args){
         //new TicTacToeGame( new HumanPlayer(), new BrainDeadPlayer()).play();
-        new TicTacToeGame( new HumanPlayer(), new RandomSmarterPlayer()).play();
+        new TicTacToeGame( new HumanPlayer(), new SmartPlayer()).play();
     }
     
 }
