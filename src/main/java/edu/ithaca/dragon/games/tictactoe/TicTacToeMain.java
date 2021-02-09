d9eb3383e34a4dfd4e0b14768064598a7bb48316
Morgan <morgan.e.ziegler@gmail.com>
2021-02-09 14:31:32 -0500

    Changed name of Agent

--- src/main/java/edu/ithaca/dragon/games/tictactoe/TicTacToeMain.java
+++ src/main/java/edu/ithaca/dragon/games/tictactoe/TicTacToeMain.java
@@ -3,14 +3,12 @@ package edu.ithaca.dragon.games.tictactoe;
 import edu.ithaca.dragon.games.tictactoe.player.BrainDeadPlayer;
 import edu.ithaca.dragon.games.tictactoe.player.HumanPlayer;
 import edu.ithaca.dragon.games.tictactoe.player.RandomMovePlayer;
-import edu.ithaca.dragon.games.tictactoe.player.RandomSmarterPlayer;
-import edu.ithaca.dragon.games.tictactoe.player.RuleBasedAgent;
-import edu.ithaca.dragon.games.tictactoe.player.SmartPlayer;
+import edu.ithaca.dragon.games.tictactoe.player.RuleBasedAgentMorgan;
 
 public class TicTacToeMain {
     public static void main (String[] args){
         //new TicTacToeGame( new HumanPlayer(), new BrainDeadPlayer()).play();
-        new TicTacToeGame( new HumanPlayer(), new SmartPlayer()).play();
+        new TicTacToeGame( new HumanPlayer(), new RuleBasedAgentMorgan()).play();
     }
     
 }
--- src/main/java/edu/ithaca/dragon/games/tictactoe/player/RuleBasedAgentMorgan.java
+++ src/main/java/edu/ithaca/dragon/games/tictactoe/player/RuleBasedAgentMorgan.java
@@ -3,7 +3,7 @@ import edu.ithaca.dragon.games.tictactoe.board.TicTacToeBoard;
 import java.util.Random; 
 import org.javatuples.Pair;
 
-public class SmartPlayer implements TicTacToePlayer{
+public class RuleBasedAgentMorgan implements TicTacToePlayer{
     @Override
     public Pair<Integer, Integer> chooseSquare(TicTacToeBoard curBoard, char yourSymbol) {
        //vertical
@@ -55,7 +55,7 @@ public class SmartPlayer implements TicTacToePlayer{
             return new Pair<>(2,0);
         }
     
-        //vertical opponent block  STILL FIXU
+        //vertical opponent block  
        for(int x=0; x<3; x++){
         if ((curBoard.checkSquare(new Pair<>(x,0)) != yourSymbol) && curBoard.checkSquare(new Pair<>(x,0)) != ' ' && curBoard.checkSquare(new Pair<>(x,1)) != yourSymbol && curBoard.checkSquare(new Pair<>(x,1)) != ' ' && curBoard.isSquareOpen(new Pair<>(x,2)) ){
             return new Pair<>(x,2);
@@ -100,12 +100,25 @@ public class SmartPlayer implements TicTacToePlayer{
     if (curBoard.checkSquare(new Pair<>(1,1)) != ' ' && curBoard.checkSquare(new Pair<>(1,1)) != yourSymbol && curBoard.checkSquare(new Pair<>(0,2)) == curBoard.checkSquare(new Pair<>(1,1)) && curBoard.isSquareOpen(new Pair<>(2,0))){
         return new Pair<>(2,0);
     }
-    
-    
-    
-    if (curBoard.isSquareOpen(new Pair<>(1,1))){
-        return new Pair<>(1,1);
+
+    //Opposite corner placements
+    if (curBoard.checkSquare(new Pair<>(0,0)) != yourSymbol && curBoard.checkSquare(new Pair<>(0,0)) != ' ' && curBoard.isSquareOpen(new Pair<>(2,2))){
+        return new Pair<>(2,2);
     }
+    if (curBoard.checkSquare(new Pair<>(0,2)) != yourSymbol && curBoard.checkSquare(new Pair<>(0,2)) != ' ' && curBoard.isSquareOpen(new Pair<>(2,0))){
+        return new Pair<>(2,0);
+    }
+    if (curBoard.checkSquare(new Pair<>(2,0)) != yourSymbol && curBoard.checkSquare(new Pair<>(2,0)) != ' ' && curBoard.isSquareOpen(new Pair<>(0,2))){
+        return new Pair<>(0,2);
+    }
+    if (curBoard.checkSquare(new Pair<>(2,2)) != yourSymbol && curBoard.checkSquare(new Pair<>(2,2)) != ' ' && curBoard.isSquareOpen(new Pair<>(0,0))){
+        return new Pair<>(0,0);
+    }
+    
+    
+    
+    
+    
 
     //place in corner
     if (curBoard.isSquareOpen(new Pair<>(0,0))){
@@ -122,7 +135,9 @@ public class SmartPlayer implements TicTacToePlayer{
     }
 
     //place in center
-    
+    if (curBoard.isSquareOpen(new Pair<>(1,1))){
+        return new Pair<>(1,1);
+    }
     
 
     //place in non corners
